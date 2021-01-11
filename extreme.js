58ef99272f94d2b8e5521e2e181b43fa83226fdd
郭思秋 <v_sqguo@digitalgd.com.cn>
2021-01-11 16:54:17 +0800

    [+] 增加协议检测，已经安装就直接打开 [+] 增加激进模式，安装了就直接用Edge打开，并尝试关闭IE

--- extreme.js
+++ extreme.js
@@ -125,37 +125,17 @@
   if (!!window.ActiveXObject || 'ActiveXObject' in window) {
     const proto = 'microsoft-edge:'
     var href = proto + window.location.href
-    window.UPTOEDG = {}
-    window.UPTOEDG.close = function () {
-      document.body.removeChild(window.UPTOEDG.wrap)
-    }
-    window.UPTOEDG.handler = function () {
-      window.protocolCheck(href, function () {
-        var href = 'https://go.microsoft.com/fwlink/?linkid=2108834&Channel=Stable&language=zh-cn'
-        var btn = document.createElement('a')
-        btn.href = href
-        btn.innerHTML = 'Download Edge'
-        btn.style.display = 'none'
-        document.body.appendChild(btn)
-        btn.click()
-        document.body.removeChild(btn)
-      })
-      window.UPTOEDG.close()
-    };
-    var html
-      = '<div style="position: fixed;bottom: 0;z-index: 2147483647;width: 100vw;font-family: 微软雅黑;">'
-      + '<div style="margin: 0 auto;width: 590px;border: solid 1px #000;font-size: 14px;color: #000;background-color: #fff;">'
-      + '<div style="border-top: solid 6px #f1ad0c;padding: 8px 16px;overflow: hidden;line-height: 2;">'
-      + '<span style="margin-right: 120px;">该浏览器无法使用完整功能。请使用最新的<a style="color: #1890ff;text-decoration: underline;" href="https://www.microsoft.com/zh-cn/edge">Edge浏览器</a></span>'
-      + '<a onclick="UPTOEDG.handler()" style="color: #1890ff;margin-right: 8px;text-decoration: none;" href="javascript:">我知道了</a>'
-      + '<svg onclick="UPTOEDG.close()" style="width:16px;height:16px;position: relative;top: 2px;" viewBox="0 0 24 24">'
-      + '<path fill="#a0a0a0" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />'
-      + '</svg>'
-      + '</div>'
-      + '</div>'
-      + '</div>'
-    window.UPTOEDG.wrap = document.createElement('div')
-    window.UPTOEDG.wrap.innerHTML = html
-    document.body.appendChild(window.UPTOEDG.wrap)
-  }
+    window.protocolCheck(href, function () {
+      var href = 'https://go.microsoft.com/fwlink/?linkid=2108834&Channel=Stable&language=zh-cn'
+      var btn = document.createElement('a')
+      btn.href = href
+      btn.innerHTML = 'Download Edge'
+      btn.style.display = 'none'
+      document.body.appendChild(btn)
+      btn.click()
+      document.body.removeChild(btn)
+    }, function() {
+      window.close()
+    })
+  };
 })();
